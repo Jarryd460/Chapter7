eddb7c3b3d3e4c315619e503b89212747bdfd40b
Yusra94 <yusraismail17@gmail.com>
2015-05-22 10:21:03 +0200

    Chapter7

---
+++ hospitalsystem/src/main/java/za/ac/cput/hospitalsystem/api/HomePage.java
@@ -0,0 +1,16 @@
+package za.ac.cput.hospitalsystem.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by student on 2015/05/13.
+ */
+@RestController
+@RequestMapping("/api/**")
+public class HomePage {
+
+}
+
+
